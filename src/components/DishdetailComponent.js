21621f425db4ec83b449fcff57adc1ffbfafba0c
Handian Santoso <handian.santoso@gmail.com>
2019-12-09 23:07:22 +0800

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import {baseUrl} from '../shared/baseUrl';
-
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || val.length <= len;
 const minLength = (len) => (val) => !(val) || val.length >= len;
@@ -88,14 +88,20 @@ class CommentForm extends Component {
     
 function RenderDish({props, dish}) { 
         if(dish != null){
-            return(                
-                <Card className="col-12 col-md-5 m-1">
+            return(          
+                <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>      
+                <Card>
                     <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                     <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>
                     </CardBody>
                 </Card>
+                </FadeTransform>
             );
         }
         else {
@@ -107,15 +113,19 @@ function RenderDish({props, dish}) {
 
     function RenderComments({comments, postComment, dishId}) {
         if(comments != null) {
-            const comment = comments.map((comment)=>{
+            const comment =
+            <Stagger in> 
+            {comments.map((comment)=>{
                 return(
+                    <Fade in>
                 <li key={comment.id}>
                     <p>{comment.comment}</p>
                     <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                 </li>
-                
+                </Fade>
                 );
-            });
+            })}
+            </Stagger>
             return(
                 <div className="col-12 col-md-5 m-1 text-left">
                     <h4>Comments</h4>
